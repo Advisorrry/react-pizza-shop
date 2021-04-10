0bda2de5facdbb45cd060e77962c8c1f37656dee
Advisorrry <sypikeh@yandex.com>
2021-04-10 13:12:21 +0500

    add fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 import { Header } from './components/Header'
@@ -5,12 +6,28 @@ import { CartPage } from './pages/CartPage'
 import { HomePage } from './pages/HomePage'
 
 function App() {
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        const  getData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/db.json')
+                const {pizzas} = response.data
+        
+                setData(pizzas)
+            } catch (e) {
+                throw new Error(e)
+            }
+        }
+        getData()
+    }, [])
+
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
                 <Switch>
-                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/" render={() => <HomePage data={data} />} />
                     <Route exact path="/cart" component={CartPage} />
 
                     <Redirect to="/" />
