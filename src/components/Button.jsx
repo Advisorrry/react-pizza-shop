72b466ffcbc09d4931a159dd849bcd89d7f33a61
Advisorrry <sypikeh@yandex.com>
2021-04-17 19:02:32 +0500

    add new props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,16 @@
 import classNames from 'classnames'
 import React from 'react'
 
-export const Button = ({children, outline, add, cart, onClick}) => {
+export const Button = ({children, outline, add, cart, onClick, black, pay, goBack, circle}) => {
     return (
         <button onClick={onClick} className={classNames('button', {
             'button--outline': outline,
             'button--add': add,
-            'button--cart': cart
+            'button--cart': cart,
+            'button--black': black,
+            'pay-btn': pay,
+            'go-back-btn': goBack,
+            'button--circle': circle,
         })}>{children}</button>
     )
 }
