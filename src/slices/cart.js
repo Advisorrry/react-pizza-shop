d15322bcef4d552ad64c170848d44d6c3621f456
Advisorrry <sypikeh@yandex.com>
2021-04-18 14:08:34 +0500

    added action to add pizza to cart

--- src/slices/cart.js
+++ src/slices/cart.js
@@ -3,22 +3,30 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     items: {},
     totalPrice: 0,
-    totalCount: 0
+    totalCount: 0,
 }
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        setTotalPrice:(state, {payload}) => {
+        addToCart: (state, { payload }) => {
+            if (!state.items[payload.id]) {
+                state.items[payload.id] = []            
+            }
+            state.items[payload.id].push(payload)
+
+            state.totalCount++
+            state.totalPrice += payload.price
+        },
+        setTotalPrice: (state, { payload }) => {
             state.totalPrice = payload
         },
-        setTotalCount:(state, {payload}) => {
+        setTotalCount: (state, { payload }) => {
             state.totalCount = payload
         },
-        
     },
 })
-export const { setTotalPrice, setTotalCount } = cartSlice.actions
+export const { setTotalPrice, setTotalCount, addToCart } = cartSlice.actions
 
 export default cartSlice.reducer
